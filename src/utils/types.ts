4f0fdb8591b3d52ff897e6c1c0a1a79cf03b515e
Samsonium <samsonovsa@kamaz.ru>
2024-07-08 17:19:26 +0300

    feat: add struct typedef

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,1 +1,10 @@
+/** Some type alias for more code reading comfort */
 export type Byte = number;
+
+/** Struct definition for Struct class constructor */
+export type StructDef = {
+    [field: string]: [
+        NumberConstructor | BooleanConstructor,
+        number
+    ] | StructDef
+};
